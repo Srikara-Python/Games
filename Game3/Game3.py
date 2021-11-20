a9924a85a1b05e9864095a507d3c4d93b051e92f
Srikara Narasimha <srikara07.sn@hmail.com>
2021-11-20 18:04:09 +0530

    Update Game3.py

--- Game3/Game3.py
+++ Game3/Game3.py
@@ -17,39 +17,39 @@ def isGameOver(board, size):
     return True
 
 
-'''将空白Cell左边的Cell右移到空白Cell位置'''
+
 def moveR(board, blank_cell_idx, num_cols):
     if blank_cell_idx % num_cols == 0: return blank_cell_idx
     board[blank_cell_idx-1], board[blank_cell_idx] = board[blank_cell_idx], board[blank_cell_idx-1]
     return blank_cell_idx - 1
 
 
-'''将空白Cell右边的Cell左移到空白Cell位置'''
+
 def moveL(board, blank_cell_idx, num_cols):
     if (blank_cell_idx+1) % num_cols == 0: return blank_cell_idx
     board[blank_cell_idx+1], board[blank_cell_idx] = board[blank_cell_idx], board[blank_cell_idx+1]
     return blank_cell_idx + 1
 
 
-'''将空白Cell上边的Cell下移到空白Cell位置'''
+
 def moveD(board, blank_cell_idx, num_cols):
     if blank_cell_idx < num_cols: return blank_cell_idx
     board[blank_cell_idx-num_cols], board[blank_cell_idx] = board[blank_cell_idx], board[blank_cell_idx-num_cols]
     return blank_cell_idx - num_cols
 
 
-'''将空白Cell下边的Cell上移到空白Cell位置'''
+
 def moveU(board, blank_cell_idx, num_rows, num_cols):
     if blank_cell_idx >= (num_rows-1) * num_cols: return blank_cell_idx
     board[blank_cell_idx+num_cols], board[blank_cell_idx] = board[blank_cell_idx], board[blank_cell_idx+num_cols]
     return blank_cell_idx + num_cols
 
 
-'''获得打乱的拼图'''
+
 def CreateBoard(num_rows, num_cols, num_cells):
     board = []
     for i in range(num_cells): board.append(i)
-    # 去掉右下角那块
+
     blank_cell_idx = num_cells - 1
     board[blank_cell_idx] = -1
     for i in range(cfg.NUMRANDOM):
@@ -62,18 +62,18 @@ def CreateBoard(num_rows, num_cols, num_cells):
     return board, blank_cell_idx
 
 
-'''随机选取一张图片'''
+
 def GetImagePath(rootdir):
     imagenames = os.listdir(rootdir)
     assert len(imagenames) > 0
     return os.path.join(rootdir, random.choice(imagenames))
 
 
-'''显示游戏结束界面'''
+
 def ShowEndInterface(screen, width, height):
     screen.fill(cfg.BACKGROUNDCOLOR)
     font = pygame.font.Font(cfg.FONTPATH, width//15)
-    title = font.render('恭喜! 你成功完成了拼图!', True, (233, 150, 122))
+    title = font.render('', True, (233, 150, 122))
     rect = title.get_rect()
     rect.midtop = (width/2, height/2.5)
     screen.blit(title, rect)
@@ -86,14 +86,14 @@ def ShowEndInterface(screen, width, height):
         pygame.display.update()
 
 
-'''显示游戏开始界面'''
+
 def ShowStartInterface(screen, width, height):
     screen.fill(cfg.BACKGROUNDCOLOR)
     tfont = pygame.font.Font(cfg.FONTPATH, width//4)
     cfont = pygame.font.Font(cfg.FONTPATH, width//20)
-    title = tfont.render('拼图游戏', True, cfg.RED)
-    content1 = cfont.render('按H或M或L键开始游戏', True, cfg.BLUE)
-    content2 = cfont.render('H为5*5模式, M为4*4模式, L为3*3模式', True, cfg.BLUE)
+    title = tfont.render('', True, cfg.RED)
+    content1 = cfont.render('', True, cfg.BLUE)
+    content2 = cfont.render('', True, cfg.BLUE)
     trect = title.get_rect()
     trect.midtop = (width/2, height/10)
     crect1 = content1.get_rect()
@@ -117,39 +117,39 @@ def ShowStartInterface(screen, width, height):
 
 '''主函数'''
 def main():
-    # 初始化
+    # 
     pygame.init()
     clock = pygame.time.Clock()
-    # 加载图片
+    # 
     game_img_used = pygame.image.load(GetImagePath(cfg.PICTURE_ROOT_DIR))
     game_img_used = pygame.transform.scale(game_img_used, cfg.SCREENSIZE)
     game_img_used_rect = game_img_used.get_rect()
-    # 设置窗口
+    # 
     screen = pygame.display.set_mode(cfg.SCREENSIZE)
     pygame.display.set_caption('拼图游戏 —— Charles的皮卡丘')
-    # 游戏开始界面
+    # 
     size = ShowStartInterface(screen, game_img_used_rect.width, game_img_used_rect.height)
     assert isinstance(size, int)
     num_rows, num_cols = size, size
     num_cells = size * size
-    # 计算Cell大小
+    # 
     cell_width = game_img_used_rect.width // num_cols
     cell_height = game_img_used_rect.height // num_rows
-    # 避免初始化为原图
+    # 
     while True:
         game_board, blank_cell_idx = CreateBoard(num_rows, num_cols, num_cells)
         if not isGameOver(game_board, size):
             break
-    # 游戏主循环
+    # 
     is_running = True
     while is_running:
-        # --事件捕获
+        # --
         for event in pygame.event.get():
-            # ----退出游戏
+            # ----
             if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
-            # ----键盘操作
+            # ----
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT or event.key == ord('a'):
                     blank_cell_idx = moveL(game_board, blank_cell_idx, num_cols)
@@ -159,7 +159,7 @@ def main():
                     blank_cell_idx = moveU(game_board, blank_cell_idx, num_rows, num_cols)
                 elif event.key == pygame.K_DOWN or event.key == ord('s'):
                     blank_cell_idx = moveD(game_board, blank_cell_idx, num_cols)
-            # ----鼠标操作
+            # ----
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 x, y = pygame.mouse.get_pos()
                 x_pos = x // cell_width
@@ -173,11 +173,11 @@ def main():
                     blank_cell_idx = moveU(game_board, blank_cell_idx, num_rows, num_cols)
                 elif idx == blank_cell_idx-num_cols:
                     blank_cell_idx = moveD(game_board, blank_cell_idx, num_cols)
-        # --判断游戏是否结束
+        # --
         if isGameOver(game_board, size):
             game_board[blank_cell_idx] = num_cells - 1
             is_running = False
-        # --更新屏幕
+        # --
         screen.fill(cfg.BACKGROUNDCOLOR)
         for i in range(num_cells):
             if game_board[i] == -1:
@@ -193,7 +193,7 @@ def main():
             pygame.draw.line(screen, cfg.BLACK, (0, i*cell_height), (game_img_used_rect.width, i*cell_height))
         pygame.display.update()
         clock.tick(cfg.FPS)
-    # 游戏结束界面
+    # 
     ShowEndInterface(screen, game_img_used_rect.width, game_img_used_rect.height)
 
 
